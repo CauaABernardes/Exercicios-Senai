e5a9c10099fa4ccfeb86f7cdbb7db1b0ed03445e
Cauã Araújo Bernardes <cauabernardess@gmail.com>
2025-09-24 11:01:53 -0300

    Delete LOP/Lista1-2 directory

--- LOP/Lista1-2/exercicio7.py
+++
@@ -1,8 +0,0 @@
-ano_nascimento = int(input("Digie o ano que você nasceu: "))
-
-if ano_nascimento > 2025:
-    print("Ano inválido")
-elif ano_nascimento <= 2009:
-    print("Você pode votar")
-else:
-    print("Você não pode votar")
